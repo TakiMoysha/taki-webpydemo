12bd5583ff3d6248d2fd118caf8962793e3fac22
TakiMoysha <36836047+TakiMoysha@users.noreply.github.com>
2025-04-23 17:45:56 +0200

    added entrypoint script and updated compose file

---
+++ tooling/scripts/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/env bash
+
+function postgres_wait() {
+  local host=${POSTGRES_HOST:-localhost}
+  local port=${POSTGRES_PORT:-5432}
+  local timeout=${TIMEOUT:-10}
+  local start_time=$(date +%s)
+
+  netcat -z -w $timeout $host $port
+
+  while [ $? -ne 0 ] && [ $(( $(date +%s) - $start_time )) -lt $timeout ]; do
+    sleep 1
+    netcat -z -w $timeout $host $port
+  done
+
+  if [ $(( $(date +%s) - $start_time )) -ge $timeout ]; then
+    echo "Postgres is not available after $timeout seconds"
+    exit 1
+  fi
+}
